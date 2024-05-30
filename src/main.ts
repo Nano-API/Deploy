ac57f94d10a7e374958831d1a9114b977762959e
Joel Milligan <joeldevelops@gmail.com>
2024-05-30 09:06:49 +0200

    Run build API call

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,11 @@ export const run = async (): Promise<void> => {
   try {
     const buildInput = await apiService.getBuildInput();
     core.info(
-      `Build prepared for ${buildInput.repoName}(${buildInput.commitSHA})`
+      `Build prepared for ${buildInput.repoName} (${buildInput.commitSHA})`
     );
+
+    const buildUrl = await apiService.createBuild(buildInput);
+    core.info(`Build started, you can watch it at ${buildUrl}`);
   } catch (error: any) {
     core.error(error.message);
     core.setFailed(error.message);
