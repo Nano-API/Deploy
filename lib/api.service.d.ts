f5bc154da9a53ca9fecebb2e0fca09be114b5ef2
florianbgt <florian.bigot321@gmail.com>
2024-09-26 17:34:18 +0200

    release v1

--- lib/api.service.d.ts
+++ lib/api.service.d.ts
@@ -1,3 +1,3 @@
-import { BuildResponse } from './types';
-export declare const createBuild: () => Promise<BuildResponse>;
+import { BuildResponse, Stack } from './types';
+export declare function createBuildV2FromStack(stack: Stack, repositoryId: Number, commitSha: string, token: string, apiKey: string): Promise<BuildResponse>;
 export declare const watchBuild: (buildId: string) => Promise<void>;
---
+++ lib/stack.d.ts
@@ -0,0 +1,2 @@
+import { Stack } from './types';
+export declare function getStack(name: string): Stack;
